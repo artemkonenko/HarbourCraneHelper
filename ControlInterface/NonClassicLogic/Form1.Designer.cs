597c07b4188d2e2352f7066103c96610295f115f
Medeuz <crobat@mail.ru>
2014-06-17 23:54:12 +0400

    Учимся юзать нормально гит...

---
+++ ControlInterface/NonClassicLogic/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace NonClassicLogic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.topViewPicture = new System.Windows.Forms.PictureBox();
+            this.sidewayViewPicture = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.topViewPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sidewayViewPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(13, 13);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(95, 12);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            // 
+            // topViewPicture
+            // 
+            this.topViewPicture.Location = new System.Drawing.Point(13, 140);
+            this.topViewPicture.Name = "topViewPicture";
+            this.topViewPicture.Size = new System.Drawing.Size(407, 242);
+            this.topViewPicture.TabIndex = 2;
+            this.topViewPicture.TabStop = false;
+            // 
+            // sidewayViewPicture
+            // 
+            this.sidewayViewPicture.Location = new System.Drawing.Point(426, 140);
+            this.sidewayViewPicture.Name = "sidewayViewPicture";
+            this.sidewayViewPicture.Size = new System.Drawing.Size(470, 242);
+            this.sidewayViewPicture.TabIndex = 3;
+            this.sidewayViewPicture.TabStop = false;
+            this.sidewayViewPicture.Click += new System.EventHandler(this.sidewayViewPicture_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Вид сверху";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(423, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Вид сбоку";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 392);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sidewayViewPicture);
+            this.Controls.Add(this.topViewPicture);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Повелитель кранов 0.01";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.topViewPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sidewayViewPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.PictureBox topViewPicture;
+        private System.Windows.Forms.PictureBox sidewayViewPicture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ControlInterface/NonClassicLogic/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NonClassicLogic
+{
+    public partial class Form1 : Form
+    {
+        //оступ от границы pictureBox'a при отрисовки, по длине
+        const int widthIndent = 40;
+        //пусть 1 метр = 3 пикселям, тогда максимальная высота волны = 24 пикселя
+        const int testWave = 24;
+
+
+        //битмап вида сбоку
+        static Bitmap sideway;
+        //графика вида сбоку
+        static Graphics sidewayGraphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //инициализация вида сбоку
+            sideway = new Bitmap(sidewayViewPicture.Size.Width, sidewayViewPicture.Size.Height);
+            sidewayGraphics = Graphics.FromImage(sideway);
+            sidewayViewPicture.Image = sideway;
+            drawWaveSideway(testWave);
+            //отрисовка корабля
+            drawShipSideway(testWave);
+            //отрисовка люльки крана и груза
+            drawCraneWithCargoSideWay(sideway.Size.Width/2 - 40);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sidewayViewPicture_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        //отрисовка корабля в виде сбоку, высота волны входной параметр
+        private void drawShipSideway(int waveHeight = 0)
+        {
+            // Create points that define polygon.
+            Point point1 = new Point(0 + widthIndent, sideway.Size.Height - 40 - waveHeight);
+            Point point2 = new Point(0 + 40 + widthIndent, sideway.Size.Height - waveHeight);
+            Point point3 = new Point(sideway.Size.Width - 40 - widthIndent, sideway.Size.Height - waveHeight);
+            Point point4 = new Point(sideway.Size.Width - widthIndent, sideway.Size.Height - 40 - waveHeight);
+            Point point5 = new Point(sideway.Size.Width - 40 - widthIndent, sideway.Size.Height - 40 - waveHeight);
+            Point point6 = new Point(sideway.Size.Width - 40 - widthIndent, sideway.Size.Height - 80 - waveHeight);
+            Point point7 = new Point(sideway.Size.Width - 80 - widthIndent, sideway.Size.Height - 80 - waveHeight);
+            Point point8 = new Point(sideway.Size.Width - 80 - widthIndent, sideway.Size.Height - 40 - waveHeight);
+            Point[] curvePoints =
+            {
+                 point1,
+                 point2,
+                 point3,
+                 point4,
+                 point5,
+                 point6,
+                 point7,
+                 point8
+            };
+            //заполнить область корабля белым цветом
+            sidewayGraphics.FillPolygon(new SolidBrush(Color.White), curvePoints);
+            //отрисовать корабль
+            sidewayGraphics.DrawPolygon(new Pen(Color.Black, 3), curvePoints);
+            //обновить изображение
+            Invalidate();
+        }
+
+        //отрисовка волны
+        private void drawWaveSideway(int waveHeight = 0)
+        {
+            Point[] curvePoints = new Point[10];
+            int step = sideway.Size.Width / 4;
+            int highPoints = 0;
+            int lowPoints = 0;
+            for (int i = 1; i < 10; i += 2) 
+            { 
+                curvePoints[i - 1] = new Point(lowPoints, sideway.Size.Height);
+                curvePoints[i] = new Point(highPoints, sideway.Size.Height - waveHeight);
+                highPoints += step;
+                lowPoints += step;
+            }
+            sidewayGraphics.DrawCurve(new Pen(Color.Blue, 3), curvePoints);
+            Invalidate();
+        }
+
+        //отрисовка люльки крана и груза (позиция люльки крана, смещение груза из-за ветра, расстояние от люльки до груза, по умолчанию 5 метров)
+        private void drawCraneWithCargoSideWay(int cranePos = 0, int deltaWind = 0, int distance = 15)
+        {
+            //отрисовка люльки
+            Point point1 = new Point(cranePos, 0);
+            Point point2 = new Point(cranePos + 80, 0);
+            Point point3 = new Point(cranePos + 80, 40);
+            Point point4 = new Point(cranePos, 40);
+            Point[] curvePoints = 
+            {
+                point1,
+                point2,
+                point3,
+                point4
+            };
+            sidewayGraphics.FillPolygon(new SolidBrush(Color.White), curvePoints);
+            sidewayGraphics.DrawPolygon(new Pen(Color.Black, 3), curvePoints);
+            Invalidate();
+
+            //отрисовка груза (длина = 36, высота = 9, точка соединения = 18)
+            Point p1 = new Point(cranePos + deltaWind + 40 - 18, 40 + distance);
+            Point p2 = new Point(cranePos + deltaWind + 40 - 18, 40 + 9 + distance);
+            Point p3 = new Point(cranePos + deltaWind  + 40 + 18, 40 + 9 + distance);
+            Point p4 = new Point(cranePos + deltaWind  + 40 + 18, 40 + distance);
+            Point[] cargoPoints = 
+            {
+                p1,
+                p2,
+                p3,
+                p4
+            };
+            sidewayGraphics.FillPolygon(new SolidBrush(Color.White), cargoPoints);
+            sidewayGraphics.DrawPolygon(new Pen(Color.Black, 3), cargoPoints);
+            Invalidate();
+
+            //отрисовка каната от люльки до груза
+            sidewayGraphics.DrawLine(new Pen(Color.Black, 3), new Point(cranePos + 40, 40), new Point(cranePos + 40 + deltaWind, 40 + distance));
+        }
+    }
+}
