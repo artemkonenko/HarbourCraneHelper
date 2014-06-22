0dffeaf59ac91252f80178cf014c8acfaa8a6f9b
Hequs <alexpetr-mail@yandex.ru>
2014-06-22 23:54:24 +0400

    FULL

---
+++ Skorohod/Skorohod/FuzzyGraph.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skorohod
+{
+    class FuzzyGraph
+    {
+        private List<FuzzyTrapeze> _list;
+
+        public FuzzyGraph(int count)
+        {
+            this._list = new List<FuzzyTrapeze>(new FuzzyTrapeze[count]);
+        }
+
+        public void addFuzzyTrapeze(int type, FuzzyTrapeze ft)
+        {
+            this._list[type] = ft;
+        }
+
+        public List<double> getFuzzyDistribution(double x)
+        {
+            List<double> res = new List<double>(new double[this._list.Count]);
+            for (int i = 0; i < this._list.Count; ++i) 
+            {
+                FuzzyTrapeze c = this._list[i];
+                if (x <= c.bottomLeft || c.bottomRight <= x)
+                {
+                    res[i] = 0;
+                    continue;
+                }
+
+                if (c.topLeft <= x && x <= c.topRight)
+                {
+                    res[i] = 1;
+                    continue;
+                }
+
+                if (c.bottomLeft < x && x < c.topLeft)
+                {
+                    res[i] = (x - c.bottomLeft) / (c.topLeft - c.bottomLeft);
+                    continue;
+                }
+
+                res[i] = 1 - (x - c.topRight) / (c.bottomRight - c.topRight); 
+            }
+            
+            return res;
+        }
+
+        public List<Point> getPolygon(List<double> distribution)
+        {
+            List<Point> res = new List<Point>();
+            if (distribution.Count != this._list.Count)
+            {
+                return null;
+            }
+            Trapeze t1 = _list[0].Cut(distribution[0]);
+            res.Add(t1.bottomLeft);
+            res.Add(t1.topLeft);
+            for (int i = 1; i < _list.Count; i++)
+            {
+                Trapeze t2 = _list[i].Cut(distribution[i]);
+                Point intersection = Point.getIntersection(t1.topRight, t1.bottomRight, t2.bottomLeft, t2.topLeft);
+                if (intersection != null)
+                {
+                    if (t1.topRight.y >= intersection.y)
+                    {
+                        res.Add(t1.topRight);
+                        if (t2.topLeft.y > intersection.y)
+                        {
+                            res.Add(t2.topLeft);
+                            res.Add(intersection);
+                        }
+                        else
+                        {
+                            res.Add(Point.getPointByTwoPointsAndY(t1.topRight, t1.bottomRight, distribution[i]));
+                        }
+                    }
+                    else
+                    {
+                        if (t2.topLeft.y > intersection.y)
+                        {
+                            res.Add(Point.getPointByTwoPointsAndY(t2.bottomLeft, t2.topLeft, distribution[i - 1]));
+                            res.Add(t2.topLeft);
+                        }
+                        else
+                        {
+                            if (t1.topRight.y > t2.topLeft.y)
+                            {
+                                res.Add(t1.topRight);
+                                res.Add(Point.getPointByTwoPointsAndY(t1.topRight, t1.bottomRight, distribution[i]));
+                            }
+                            else if (t1.topRight.y < t2.topLeft.y)
+                            {
+                                res.Add(t2.topLeft);
+                                res.Add(Point.getPointByTwoPointsAndY(t2.bottomLeft, t2.topLeft, distribution[i - 1]));
+                            }
+                        }
+                    }
+                }
+                t1 = t2;
+            }
+            res.Add(t1.topRight);
+            res.Add(t1.bottomRight);
+            return res;
+        }
+    }
+}
--- Skorohod/Skorohod/Program.cs
+++ Skorohod/Skorohod/Program.cs
@@ -10,54 +10,16 @@ namespace Skorohod
 
     class Program
     {
-        enum DeviationFuzzyTypes { UnderControl, OutControl };
-        enum HeightFuzzyTypes { Dangerous, Close, Far };
-        enum SpeedFuzzyTypes { Up, DownSlow, DownFast };
-
         static void Main(string[] args)
         {
-            /* Нечеткие правила для параметра ОТКЛОНЕНИЕ от конечной вертикали */
-            FuzzyGraph deviationGraph = new FuzzyGraph(Enum.GetNames(typeof(DeviationFuzzyTypes)).Length);
-            deviationGraph.addFuzzyTrapeze((int)DeviationFuzzyTypes.UnderControl, new FuzzyTrapeze(0, 0, 3, 5));
-            deviationGraph.addFuzzyTrapeze((int)DeviationFuzzyTypes.OutControl, new FuzzyTrapeze(3, 5, 100, 100));
+            double d = 4.5, h = 2.5;
 
-            /* Нечеткие правила для параметра РАССТОЯНИЕ до палубы */
-            FuzzyGraph heightGraph = new FuzzyGraph(Enum.GetNames(typeof(HeightFuzzyTypes)).Length);
-            heightGraph.addFuzzyTrapeze((int)HeightFuzzyTypes.Dangerous, new FuzzyTrapeze(-100, -100, 2, 3));
-            heightGraph.addFuzzyTrapeze((int)HeightFuzzyTypes.Close, new FuzzyTrapeze(2, 3, 5, 6));
-            heightGraph.addFuzzyTrapeze((int)HeightFuzzyTypes.Far, new FuzzyTrapeze(5, 6, 100, 100));
+            FuzzyLogic logic = new FuzzyLogic();
 
-            /* Нечеткие правила для параметра СКОРОСТЬ подъема */
-            FuzzyGraph speedGraph = new FuzzyGraph(Enum.GetNames(typeof(SpeedFuzzyTypes)).Length);
-            speedGraph.addFuzzyTrapeze((int)SpeedFuzzyTypes.Up, new FuzzyTrapeze(-100, -100, -1, 0));
-            speedGraph.addFuzzyTrapeze((int)SpeedFuzzyTypes.DownSlow, new FuzzyTrapeze(-1, 0, 1, 2));
-            speedGraph.addFuzzyTrapeze((int)SpeedFuzzyTypes.DownFast, new FuzzyTrapeze(1, 2, 100, 100));
+            logic.getDeviationCompensation(d, h);
+            logic.getHeightCompensation(d, h);
 
-            /* Правила */
-            int[,] rules = { 
-                           { 0, 1, 2 },         //UnderControl
-                           { 0, 0, 1 }          //OutControl
-                           };
 
-            /* Параметры */
-            double d = Math.Abs(4.5), h = 2.5;
-            FuzzyDistribution deviationDistribution = deviationGraph.getFuzzyDistribution(d);
-            FuzzyDistribution heightDistribution = heightGraph.getFuzzyDistribution(h);
-
-            /* Вычисление распределение методов возможного поведения */
-            FuzzyDistribution speedDistribution = new FuzzyDistribution(new double[Enum.GetNames(typeof(SpeedFuzzyTypes)).Length]);
-            for (int i = 0; i < deviationDistribution.Count; ++i )
-            {
-                for (int j = 0; j < heightDistribution.Count; ++j )
-                {
-                    speedDistribution[rules[i, j]] =
-                        Math.Max(speedDistribution[rules[i, j]], Math.Min(deviationDistribution[i], heightDistribution[j]));
-                }
-            }
-            
-            /* и тут появляется Янушка */
-            
-            
             System.Console.ReadLine();
         }
     }
