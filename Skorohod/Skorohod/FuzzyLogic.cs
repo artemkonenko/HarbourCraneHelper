999fbaac6e3b651417569017a47e83e1b76bb7c5
Hequs <alexpetr-mail@yandex.ru>
2014-06-22 19:25:55 +0400

    Реализация нечекости + ВЕТЕР

--- Skorohod/Skorohod/FuzzyLogic.cs
+++ Skorohod/Skorohod/FuzzyLogic.cs
@@ -19,6 +19,36 @@ namespace Skorohod
         {
             this._list.Add(type, ft);
         }
+
+        public Dictionary<int, double> getFuzzyDistribution(double x)
+        {
+            Dictionary<int, double> res = new Dictionary<int, double>(this._list.Count);
+            foreach (KeyValuePair<int, FuzzyTrapeze> entry in this._list) 
+            {
+                FuzzyTrapeze c = entry.Value;
+                if (x <= c.bottomLeft || c.bottomRight <= x)
+                {
+                    res.Add(entry.Key, 0);
+                    continue;
+                }
+
+                if (c.topLeft <= x && x <= c.topRight)
+                {
+                    res.Add(entry.Key, 1);
+                    continue;
+                }
+
+                if (c.bottomLeft < x && x < c.topLeft)
+                {
+                    res.Add(entry.Key, (x - c.bottomLeft) / (c.topLeft - c.bottomLeft));
+                    continue;
+                }
+
+                res.Add(entry.Key, 1 - (x - c.topRight) / (c.bottomRight - c.topRight)); 
+            }
+            
+            return res;
+        }
     }
 
     class FuzzyLogic
--- Skorohod/Skorohod/Program.cs
+++ Skorohod/Skorohod/Program.cs
@@ -6,16 +6,24 @@ using System.Threading.Tasks;
 
 namespace Skorohod
 {
+    using FuzzyDistribution = Dictionary<int, double>;
+
     class Program
     {
         enum DeviationFuzzyTypes { StronglyLeft, Left, Right, StronglyRight };
 
         static void Main(string[] args)
         {
-            FuzzyGraph Deviation = new FuzzyGraph();
+            /* Нечеткие правила для параметра ОТКЛОНЕНИЕ от конечной вертикали */
+            FuzzyGraph deviationGraph = new FuzzyGraph();
+            deviationGraph.addFuzzyTrapeze((int) DeviationFuzzyTypes.StronglyLeft, new FuzzyTrapeze(-100, -100, -5, -3));
+            deviationGraph.addFuzzyTrapeze((int) DeviationFuzzyTypes.Left, new FuzzyTrapeze(-5, -3, -1, 1));
+            deviationGraph.addFuzzyTrapeze((int) DeviationFuzzyTypes.Right, new FuzzyTrapeze(-1, 1, 3, 5));
+            deviationGraph.addFuzzyTrapeze((int) DeviationFuzzyTypes.StronglyRight, new FuzzyTrapeze(3, 5, 100, 100));
 
-            Deviation.addFuzzyTrapeze(DeviationFuzzyTypes.StronglyLeft, new FuzzyTrapeze(
-                )
+            FuzzyDistribution deviationDistribution = deviationGraph.getFuzzyDistribution(0);
+
+            System.Console.ReadLine();
         }
     }
 }
